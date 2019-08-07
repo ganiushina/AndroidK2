b6f0ef4b339879ad1eb799ab616a8c5bab0b741d
ganiushina <32090008+ganiushina@users.noreply.github.com>
2019-08-07 14:45:05 +0400

    Убрала все неныжные комментарии

--- app/src/main/java/ru/geekbrains/projectandroid2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/projectandroid2/MainActivity.java
@@ -1,13 +1,10 @@
 package ru.geekbrains.projectandroid2;
 
-import android.content.Intent;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -26,18 +23,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.view.Menu;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    private SensorManager sensorManager;
-    private List<android.hardware.Sensor> sensors;
-    private android.hardware.Sensor sensorLight;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,19 +72,15 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
